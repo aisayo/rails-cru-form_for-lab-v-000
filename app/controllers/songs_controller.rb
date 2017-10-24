70b1922091276be84cf1e59d2a81ec4492937e45
aisayo <aisayo@icloud.com>
2017-10-24 23:00:12 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,9 +13,6 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params(:name, :artist_id, :genre_id))
-    @song.name = params[:song][:name]
-    @song.artist = params[:song][:artist_id]
-    @song.genre = params[:song][:genre_id]
     @song.save
     redirect_to song_path(@song)
   end
