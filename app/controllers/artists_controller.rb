fe49bd85553fd68e4da6b87ec06411d7a1028c76
aisayo <aisayo@icloud.com>
2017-10-24 21:42:34 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -11,6 +11,9 @@ class ArtistsController < ApplicationController
   def create
     @artist = Artist.new
     @artist.name = params[:artist][:name]
+    @artist.bio = params[:artist][:bio]
+    @artist.save
+    redirect_to artist_path(@artist)
   end
 
 
